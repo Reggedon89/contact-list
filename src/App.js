15593460fb03847326990cc55d3025f870049fcd
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-29 12:28:26 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div class="contactList">
-        <ul>
-          <li>Contacts</li>
-          <li />
-          <li />
-          <li />
-          <li />
-        </ul>
+      <div class="container">
+        
       </div>
     );
   }
